030dfa21782f7bc294c0698b6ce12d097856dddc
Yassine CHARJAMI <yassine.charjami@gmail.com>
2026-08-08 21:22:14 +0200

    initial commit

---
+++ src/main/java/com/data/api/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.data.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Data API")
+                        .version("0.0.1")
+                        .description("API for managing data"));
+    }
+    
+}
